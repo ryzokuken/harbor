708c69904a7b856905ca37cb3e581133306d3665
Ujjwal Sharma <usharma1998@gmail.com>
2017-10-02 02:52:55 +0530

    Redesign app

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
+});
