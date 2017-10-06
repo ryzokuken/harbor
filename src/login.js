ae9771e6f7f8bfcefc704682a687ba08874d6936
Ujjwal Sharma <usharma1998@gmail.com>
2017-10-06 12:06:36 +0530

    Add a loading animation

--- src/login.js
+++ src/login.js
@@ -1,7 +1,10 @@
 const {ipcRenderer: ipc} = require('electron');
 
+const spinnerHTML = '<div class="spinner"><div class="cube1"></div><div class="cube2"></div></div>';
+
 document.getElementById('btn-login').addEventListener('click', () => {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
   ipc.send('login', {username, password});
+  document.getElementsByClassName('container')[0].innerHTML = spinnerHTML;
 });
