1e8328d0767dae791d97cf0065242d05c70db451
Ujjwal Sharma <usharma1998@gmail.com>
2018-02-04 23:32:05 +0530

    Add a login component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,45 @@
 <template>
-  <div id="app">
-    <landing-page></landing-page>
-  </div>
+  <login></login>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage';
+  import Login from '@/components/Login';
 
   export default {
     name: 'harbor',
     components: {
-      LandingPage,
+      Login,
     },
   };
 </script>
 
 <style>
-  /* CSS */
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background: linear-gradient(to right, #00b09b, #96c93d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+
+  input {
+    padding: 10px;
+    margin: 5px;
+    border-radius: 5px;
+    font-size: 2rem !important;
+    border: 2px solid white;
+    color: white;
+    background: transparent;
+    outline: none;
+  }
+
+  ::placeholder {
+    color: rgba(256, 256, 256, 0.3);
+  }
 </style>
---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <input type="text" placeholder="Username">
+    <input type="password" placeholder="Password">
+    <input type="submit" value="Login">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'login',
+};
+</script>
