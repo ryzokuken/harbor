f7b113f623ad34a3611094eabb12033de1398b81
Ujjwal Sharma <usharma1998@gmail.com>
2018-02-06 20:05:47 +0530

    Remove static files hack

--- src/main/index.js
+++ src/main/index.js
@@ -1,15 +1,5 @@
 import { app, BrowserWindow, ipcMain as ipc } from 'electron'; // eslint-disable-line
 
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path')
-    .join(__dirname, '/static')
-    .replace(/\\/g, '\\\\'); // eslint-disable-line
-}
-
 let mainWindow;
 const winURL =
   process.env.NODE_ENV === 'development'
