5c5fcdcbedf61f6cb38811cb8318809a3b51ab86
Ujjwal Sharma <usharma1998@gmail.com>
2018-02-06 19:35:31 +0530

    Prettify App.vue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -6,67 +6,71 @@
 </template>
 
 <script>
-  import Login from '@/components/Login';
-  import Logout from '@/components/Logout';
+import Login from '@/components/Login';
+import Logout from '@/components/Logout';
 
-  export default {
-    name: 'harbor',
-    components: {
-      Login,
-      Logout,
+export default {
+  name: 'harbor',
+  components: {
+    Login,
+    Logout,
+  },
+  data() {
+    return {
+      loggedIn: false,
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.loggedIn = true;
     },
-    data() {
-      return {
-        loggedIn: false,
-      };
+    handleLogout() {
+      this.loggedIn = false;
     },
-    methods: {
-      handleLogin() {
-        this.loggedIn = true;
-      },
-      handleLogout() {
-        this.loggedIn = false;
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
 
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    background: linear-gradient(to right, #00b09b, #96c93d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  }
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: linear-gradient(
+    to right,
+    #00b09b,
+    #96c93d
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
 
-  input {
-    padding: 10px;
-    margin: 5px;
-    border-radius: 5px;
-    font-size: 2rem !important;
-    border: 2px solid white;
-    color: white;
-    background: transparent;
-    outline: none;
-  }
+input {
+  padding: 10px;
+  margin: 5px;
+  border-radius: 5px;
+  font-size: 2rem !important;
+  border: 2px solid white;
+  color: white;
+  background: transparent;
+  outline: none;
+}
 
-  input[type=submit] {
-    transition: all 0.2s ease-in-out;
-  }
+input[type='submit'] {
+  transition: all 0.2s ease-in-out;
+}
 
-  input[type=submit]:hover {
-    color: #4BBD6C;
-    background: white;
-  }
+input[type='submit']:hover {
+  color: #4bbd6c;
+  background: white;
+}
 
-  ::placeholder {
-    color: rgba(256, 256, 256, 0.3);
-  }
+::placeholder {
+  color: rgba(256, 256, 256, 0.3);
+}
 </style>
